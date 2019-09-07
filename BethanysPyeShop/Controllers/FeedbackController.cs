4197aaccb41afdabd2fcf8f0f0464c2f9c6a8ec4
Renato Cardoso <rgdiascardoso@gmail.com>
2019-09-07 19:59:27 +0100

    Feedback view

---
+++ BethanysPyeShop/Controllers/FeedbackController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BethanysPyeShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BethanysPyeShop.Controllers
+{
+    public class FeedbackController : Controller
+    {
+        private readonly IFeedbackRepository _feedbackRepository;
+
+        public FeedbackController(IFeedbackRepository feedbackRepository)
+        {
+            _feedbackRepository = feedbackRepository;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
